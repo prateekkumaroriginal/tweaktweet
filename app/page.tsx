351a2804cba08020d8ba422c1121c13615631d0e
Prateek Kumar <prateek.kumar@unthinkable.co>
2025-01-31 16:32:17 +0530

    Mildly Responsive

--- app/page.tsx
+++ app/page.tsx
@@ -44,8 +44,8 @@ export default function Home() {
   return (
     <div className="h-full flex flex-col items-center gap-y-8 pt-20 px-8">
       <div className="flex flex-col items-center mb-12">
-        <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-rose-500 text-7xl">TWEAKTWEET</h1>
-        <p className="text-sky-300 text-lg">Tweak your post to maximize engagement! </p>
+        <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-rose-500 text-2xl md:text-5xl lg:text-7xl">TWEAKTWEET</h1>
+        <p className="text-sky-300 text-sm md:text-lg">Tweak your post to maximize engagement! </p>
       </div>
 
       <div className="flex flex-col items-center w-[1000px] max-w-full gap-y-4 px-2 pb-20">
